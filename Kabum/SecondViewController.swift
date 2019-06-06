20b5f0a1d663b7a99dc18adf3f898639dbaa29b5
Leonardo Bortolotti <leobortolotti@me.com>
2019-06-06 20:30:06 -0300

    Initial Commit

---
+++ Kabum/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kabum
+//
+//  Created by Leonardo Bortolotti on 06/06/19.
+//  Copyright © 2019 Kabum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
