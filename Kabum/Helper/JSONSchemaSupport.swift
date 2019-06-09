d962f00f46304b90b1c4755e9e0a8917ea755167
Leonardo Bortolotti <leobortolotti@me.com>
2019-06-09 20:25:36 -0300

    iOS mínimo suportado agora é o 10.0

--- Kabum/Helper/JSONSchemaSupport.swift
+++ Kabum/Helper/JSONSchemaSupport.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-// MARK: - Helper functions for creating encoders and decoders
-
 func newJSONDecoder() -> JSONDecoder {
     let decoder = JSONDecoder()
     if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
